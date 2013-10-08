498760dd5d947079b252a4ef4d37230741cc73fd
WanderWang <wander.wang@ismole.com>
2013-10-08 14:07:08 +0800

    generateCpp Module

--- cppgenerator.js
+++ cppgenerator.js
@@ -101,13 +101,65 @@ var generateCppText = function(fileName,nodeList){
 }
 
 
+var generateHeaderClassDefine = function(fileName,nodeList){
+    var template = "class {ClassName} : public cocos2d::CCLayer\n" +
+        ", public cocos2d::extension::CCBSelectorResolver\n" +
+        ", public cocos2d::extension::CCBMemberVariableAssigner\n" +
+        ", public cocos2d::extension::CCNodeLoaderListener"
+    return template;
+}
+
+
+var generateHeaderTemplateMethod = function (fileName,nodeList){
+    var template = "public:\n\n" + "{public}\n" + "\nprivate:\n\n" + "{private}";
+    var publicList = [
+        "void onEnter();",
+        "void onExit();",
+        "cocos2d::SEL_MenuHandler onResolveCCBCCMenuItemSelector(cocos2d::CCObject * pTarget, const char * pSelectorName);",
+        "cocos2d::extension::SEL_CCControlHandler onResolveCCBCCControlSelector(cocos2d::CCObject * pTarget, const char * pSelectorName);",
+        "bool onAssignCCBMemberVariable(cocos2d::CCObject * pTarget, const char * pMemberVariableName, cocos2d::CCNode * pNode);",
+        "void onNodeLoaded(cocos2d::CCNode * pNode, cocos2d::extension::CCNodeLoader * pNodeLoader);"
+        ];
+
+
+    var privateList = nodeList.map(function(item){
+        return  "{nodeType}{varName}"
+            .replace("{nodeType}",item.baseClass + " *")
+            .replace("{varName}",generateCppVariable(item));
+    });
+    return template
+        .replace("{public}",publicList.join("\n"))
+        .replace("{private}",privateList.join("\n"));
+}
+
+
+var generateHeaderText = function(fileName,nodeList){
+    var template = [
+        "#ifndef __CocosDragonJS__{ClassName}__",
+        "#define __CocosDragonJS__{ClassName}__",
+        "#include \"cocos2d.h\"",
+        "#include \"cocos-ext.h\"",
+        "",
+        "{ClassDefine}",
+        "{",
+        "{templateMethod}",
+//        "{businessLogicMethod}"
+        "};",
+        "#endif"
+    ].join("\n");
+    nodeList = nodeList.filter(function(node){
+        return node.memberVarAssignmentName != "";
+    });
+    return template
+        .replace("{ClassDefine}",generateHeaderClassDefine(fileName,nodeList))
+        .replace("{templateMethod}",generateHeaderTemplateMethod(fileName,nodeList))
+        .replace(/{ClassName}/gi,fileName)
+}
 
 var ccbParser = require("./ccbparser.js");
 var ccbConfig = ccbParser.getCCBData("./SelectHeroItemLayer.ccb");
 var nodeList = ccbParser.loopNodeSync(ccbConfig.nodeGraph);
-console.log (nodeList)
-
-
 //console.log (generateGlueMethod("LoginLayer",["m_pHeroSkinNode","m_pEmbattleBtn"]));
 console.log (generateCppText("LoginLayer",nodeList));
+console.log (generateHeaderText("LoginLayer",nodeList));
 
